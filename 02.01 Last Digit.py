188c7dda153f1272d00a754d2441aaef8400565f
parikshya098 <pbhandari1@ualr.edu>
2022-02-02 16:10:47 +0000

    Unit 2

---
+++ 02.01 Last Digit.py
@@ -0,0 +1,3 @@
+original_number = int(input("Enter a number: "))
+last_digit = original_number % 10
+print("{}".format(last_digit))
---
+++ 02.02 Two Digits.py
@@ -0,0 +1,3 @@
+num1 = int(input("Enter a number: "))
+x = int(input("Ones digit: "))
+y = int(input("Tens digit: "))
