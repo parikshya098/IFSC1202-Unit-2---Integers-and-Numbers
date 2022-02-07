901f9e639902a8d27f1eae07de968da647aca04e
parikshya098 <pbhandari1@ualr.edu>
2022-02-07 04:04:00 +0000

    Unit 2

---
+++  2.05 Tens Digit.py
@@ -0,0 +1,4 @@
+original_number = int(input("Enter a number: "))
+tens_digit = (original_number//10) % 10
+
+print("Tens digit : {}".format(tens_digit))
---
+++ 02.04 Last Two Digits.py
@@ -0,0 +1,5 @@
+original_number = int(input("Enter a number: "))
+last_two = original_number%100
+
+print("Last two digits : {:02d}".format(last_two))
+
