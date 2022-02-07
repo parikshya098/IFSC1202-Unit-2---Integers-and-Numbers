ee3bc2dfdaa33a8b4929a5b6ec78f20bdb840f4f
parikshya098 <pbhandari1@ualr.edu>
2022-02-07 03:40:49 +0000

    Unit 2

--- 02.02 Two Digits.py
+++ 02.02 Two Digits.py
@@ -1,7 +1,4 @@
 original_number = int(input("Enter a number: "))
 last_digit = original_number % 10
 first_digit = (original_number - last_digit) // 10
-print("Ones digit : ", last_digit)
-print("Tens digit : ", first_digit)
-
-
+print("Ones digit : {} \nTens digit : {}".format(last_digit, first_digit))
